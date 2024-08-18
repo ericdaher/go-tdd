c1955ce3fe705dbe4d86e437d8b544b12b012aa8
ericdaher <ericdaher2@gmail.com>
2024-08-18 09:53:52 -0300

    working with structs

---
+++ structs/structs_test.go
@@ -0,0 +1,31 @@
+package structs
+
+import "testing"
+
+func TestPerimeter(t *testing.T) {
+	rectangle := Rectangle{10.0, 10.0}
+	got := Perimeter(rectangle)
+	want := 40.0
+
+	if got != want {
+		t.Errorf("got %.2f want %.2f", got, want)
+	}
+}
+
+func TestArea(t *testing.T) {
+	areaTests := []struct {
+		shape   Shape
+		hasArea float64
+	}{
+		{shape: Rectangle{Width: 12, Height: 6}, hasArea: 72.0},
+		{shape: Circle{Radius: 10}, hasArea: 314.1592653589793},
+		{shape: Triangle{Base: 4, Height: 3}, hasArea: 6.0},
+	}
+
+	for _, tt := range areaTests {
+		got := tt.shape.Area()
+		if got != tt.hasArea {
+			t.Errorf("%#v got %g hasArea %g", tt.shape, got, tt.hasArea)
+		}
+	}
+}
